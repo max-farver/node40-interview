0de654549a9585e2360bcc0ad1524809885c15f4
max-farver <mfarver99@gmail.com>
2021-01-31 15:28:37 -0600

    Format endpoint with tests

--- src/main/java/com/interview/resources/HelloWorldResource.java
+++ src/main/java/com/interview/resources/HelloWorldResource.java
@@ -2,11 +2,11 @@ package com.interview.resources;
 
 import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.OptBoolean;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.OptionalInt;
 
 @Path("/hello-world")
 @Produces(MediaType.TEXT_PLAIN)
@@ -25,5 +25,31 @@ public class HelloWorldResource {
     @Timed
     public String sayHello() {
         return template;
+    }
+
+    @GET
+    @Timed
+    @Path("/format")
+    public String variableHello(@QueryParam("reverse") @DefaultValue("false") boolean reverse,
+                                @QueryParam("scream") @DefaultValue("false") boolean scream,
+                                @QueryParam("chopped") @DefaultValue("0") int charsChopped) {
+        String tempGreeting = template;
+
+        if (charsChopped > 0 && charsChopped < template.length()) {
+            tempGreeting = tempGreeting.substring(0, tempGreeting.length() - charsChopped);
+        }
+        else if (charsChopped > 0) {
+            return "";
+        }
+
+        if (reverse) {
+            tempGreeting = new StringBuffer(tempGreeting).reverse().toString();
+        }
+
+        if (scream) {
+            tempGreeting = tempGreeting.toUpperCase();
+        }
+
+        return tempGreeting;
     }
 }
--- src/test/groovy/com/interview/resources/HelloWorldResourceTest.groovy
+++ src/test/groovy/com/interview/resources/HelloWorldResourceTest.groovy
@@ -58,14 +58,126 @@ class HelloWorldResourceTest extends Specification {
 
     def "GET root with no query parameters returns plain text"() {
         given:
+        def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to root"
+        def result = client.target(String.format("http://localhost:%d/hello-world", SUPPORT.getLocalPort()))
+                .request()
+                .get(String)
+
+        then: "client receives expected greeting"
+        result == message
+    }
+
+    def "GET /format with no parameters returns default greeting"() {
+        given:
             def client = new JerseyClientBuilder().build()
 
         when: "client sends GET to root"
-            def result = client.target(String.format("http://localhost:%d/hello-world", SUPPORT.getLocalPort()))
+            def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
                     .request()
                     .get(String)
 
         then: "client receives expected greeting"
             result == message
+    }
+
+    def "GET /format with reverse as true returns reversed greeting"() {
+        given:
+        def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to /format"
+        def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
+                .queryParam("reverse", true)
+                .request()
+                .get(String)
+
+        then: "client receives expected greeting"
+        result == "?yadot uoy era woH !olleH"
+    }
+
+    def "GET /format with reverse as false returns default greeting"() {
+        given:
+            def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to /format"
+            def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
+                    .queryParam("reverse", false)
+                    .request()
+                    .get(String)
+
+        then: "client receives expected greeting"
+            result == message
+    }
+
+    def "GET /format with scream as true returns template as uppercase"() {
+        given:
+            def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to /format"
+            def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
+                    .queryParam("scream", true)
+                    .request()
+                    .get(String)
+
+        then: "client receives expected greeting"
+            result == message.toUpperCase()
+    }
+
+    def "GET /format with scream as false returns default template"() {
+        given:
+            def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to /format"
+            def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
+                    .queryParam("scream", false)
+                    .request()
+                    .get(String)
+
+        then: "client receives expected greeting"
+            result == message
+    }
+
+    def "GET /format with charsChopped as 1 returns template with last character removed"() {
+        given:
+            def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to /format"
+            def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
+                    .queryParam("chopped", 1)
+                    .request()
+                    .get(String)
+
+        then: "client receives expected greeting"
+            result == message.substring(0, message.length()-1)
+    }
+
+    def "GET /format with charsChopped as 100 returns empty string"() {
+        given:
+            def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to /format"
+            def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
+                    .queryParam("chopped", 100)
+                    .request()
+                    .get(String)
+
+        then: "client receives expected greeting"
+            result == ""
+    }
+
+    def "GET /format with reverse as true and charsChopped as 1 returns template with last character removed then reversed"() {
+        given:
+            def client = new JerseyClientBuilder().build()
+
+        when: "client sends GET to /format"
+            def result = client.target(String.format("http://localhost:%d/hello-world/format", SUPPORT.getLocalPort()))
+                    .queryParam("reverse", true)
+                    .queryParam("chopped", 1)
+                    .request()
+                    .get(String)
+
+        then: "client receives expected greeting"
+            result == "yadot uoy era woH !olleH"
     }
 }
